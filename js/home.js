3edadd830909bd0986837c012ed30b3e93230e35
Gerardo Giannetta <gerardo.giannetta@exprivia.com>
2025-01-18 01:05:11 +0100

    Work in progress...

--- js/home.js
+++ js/home.js
@@ -1,28 +1,41 @@
 import { STORAGE } from './store.js';
+
+const session = {
+    id: '',
+    surveys: [],
+    status: '',
+    creation: '',
+    lastUpdate: ''
+};
  
 const contextBtn = document.getElementById('context-btn');
+const startBtn = document.getElementById('start-btn');
 const errorDiv = document.getElementById('msg-error');
 const table = document.getElementById('table');
 const inputFile = document.getElementById('data-file');
 const newBtn = {
     btn: document.getElementById('new'),
     isClicked: false,
-    tableHeader: createTableHeader('Survey', 'Categories', 'Questions'),
-    data: STORAGE.surveys
+    tableHeader: createTableHeader('Survey', 'Description', 'Categories', 'Questions'),
+    data: STORAGE.surveys,
+    fillTableCallback: newBtnFillTable
 }
 const loadBtn = {
     btn: document.getElementById('load'),
     isClicked: false,
-    tableHeader: createTableHeader('Assessment', 'Survey', 'Status', 'Timestamp'),
-    data: STORAGE.sessions
+    tableHeader: createTableHeader('Assessment', 'Survey', 'Status', 'Creation', 'Last update'),
+    data: STORAGE.sessions,
+    fillTableCallback: loadBtnFillTable
 }
 const buttons = [newBtn, loadBtn];
 
+setContextBtn();
 inputFile.addEventListener('change', fileUpload);
 newBtn.btn.addEventListener('click', () => { clickButton(newBtn) });
 loadBtn.btn.addEventListener('click', () => { clickButton(loadBtn) });
 
 function clickButton(btn) {
+    resetMenu();
     for (const button of buttons) {
         if (button !== btn) {
             button.isClicked = false;
@@ -41,7 +54,7 @@ function clickButton(btn) {
             button.btn.style.backgroundColor = 'rgba(255, 99, 132, 0.7)';
             button.btn.style.color = 'white';
             clearAndHideTable();
-            fillAndShowTable(btn.tableHeader, btn.data);
+            fillAndShowTable(btn);
         }
     }
 }
@@ -53,7 +66,9 @@ function fileUpload(ev) {
         const reader = new FileReader();
         reader.onload = (ev2) => {
             if (STORAGE.init(ev2.target.result)) {
-                contextBtn.innerText = STORAGE.contextId;
+                newBtn.data = STORAGE.surveys;
+                loadBtn.data = STORAGE.sessions;
+                setContextBtn();
             } else {
                 showFileUploadError();
             }
@@ -66,17 +81,9 @@ function createTableHeader(...headers) {
     const tr = document.createElement('tr');
 
     let th = document.createElement('th');
-    const input = document.createElement('input'); //<input type="checkbox" value="">
+    const input = document.createElement('input');
     input.type = 'checkbox';
-    input.addEventListener('click', (ev) => {
-        const nodes = table.childNodes;
-        const size = nodes.length;
-        const checkValue = ev.target.checked;
-        for (let i = 1; i < size; ++i) {
-            const checkbox = nodes[i].childNodes[0].childNodes[0];
-            checkbox.checked = checkValue;
-        }
-    });
+    input.addEventListener('click', handleHeaderCheckboxClick);
     th.appendChild(input);
     tr.appendChild(th);
 
@@ -89,17 +96,43 @@ function createTableHeader(...headers) {
     return tr;
 }
 
+function handleHeaderCheckboxClick(ev) {
+    const nodes = table.childNodes;
+    const size = nodes.length;
+    const checkValue = ev.target.checked;
+    for (let i = 1; i < size; ++i) {
+        const checkbox = nodes[i].childNodes[0].childNodes[0];
+        checkbox.checked = checkValue;
+    }
+    if (checkValue) {
+        if (session.surveys.length > 0) { 
+            session.surveys = new Array(STORAGE.surveys.length); 
+        }
+        let i = 0;
+        for (const survey of STORAGE.surveys) {
+            session.surveys[i++] = survey.id;
+        }
+    } else {
+        session.surveys = []
+    }
+    handleStartBtn();
+}
+
 function clearAndHideTable() {
+    startBtn.style.visibility = 'hidden';
     table.style.visibility = 'hidden';
     while (table.hasChildNodes()) {
         table.removeChild(table.lastChild);
     }
 }
 
-function fillAndShowTable(headerHtmlNode, dataList) {
+function fillAndShowTable(btn) {
+    const headerHtmlNode = btn.tableHeader;
+    const dataList = btn.data;
     let tr, td, input;
 
     table.style.visibility = 'visible';
+    startBtn.style.visibility = 'visible';
     if (!dataList) {
         tr = document.createElement('tr');
         const th = document.createElement('th');
@@ -121,20 +154,74 @@ function fillAndShowTable(headerHtmlNode, dataList) {
         td = document.createElement('td');
         input = document.createElement('input');
         input.type = 'checkbox';
-        input.addEventListener('click', (ev) => {
-            if (!ev.target.checked) {
-                table.childNodes[0].childNodes[0].childNodes[0].checked = false;
-            }
-        });
+        input.addEventListener('click', handleRowCheckboxClick);
         td.appendChild(input);
         tr.appendChild(td);
-        for (const key in data) {
-            td = document.createElement('td');
-            td.innerText = data[key];
-            tr.appendChild(td);
-        }
+        btn.fillTableCallback(data, tr);
         table.appendChild(tr);
     }
+}
+
+function handleRowCheckboxClick(ev) {
+    if (!ev.target.checked) {
+        table.childNodes[0].childNodes[0].childNodes[0].checked = false;
+    }
+    if (newBtn.isClicked) {
+        handleSurveyByCheckbox(ev.target);
+        handleStartBtn();
+    }
+}
+
+function newBtnFillTable(data, tr) {
+    let td = document.createElement('td');
+    td.innerText = data.id;
+    tr.appendChild(td);
+
+    td = document.createElement('td');
+    td.innerText = data.description;
+    tr.appendChild(td);
+
+    td = document.createElement('td');
+    td.innerText = data.categories.length;
+    tr.appendChild(td);
+
+    let qCount = 0;
+    for (const cat of data.categories) {
+        qCount += cat.questions.length;
+    }
+
+    td = document.createElement('td');
+    td.innerText = qCount;
+    tr.appendChild(td);
+}
+
+function loadBtnFillTable(data, tr) {
+    for (const key in data) {
+        const td = document.createElement('td');
+        td.innerText = data[key];
+        tr.appendChild(td);
+    }
+}
+
+function handleSurveyByCheckbox(checkbox) {
+    const size = table.childNodes.length;
+    for (let i = 0; i < size; ++i) {
+        if (table.childNodes[i].childNodes[0].childNodes[0] === checkbox) {
+            const id = STORAGE.surveys[i-1].id;
+            if (!checkbox.checked) {
+                // remove session survey
+                session.surveys = session.surveys.filter(s => s !== id);
+            } else {
+                // add session survey
+                session.surveys.push(id);
+            }
+            break;
+        }
+    }
+}
+
+function setContextBtn() {
+    contextBtn.innerText = STORAGE.contextId;
 }
 
 function showFileUploadError() {
@@ -143,4 +230,31 @@ function showFileUploadError() {
 
 function resetFileUploadError() {
     errorDiv.style.display = 'none';
+}
+
+function disableStartBtn() {
+    startBtn.disabled = true;
+    startBtn.style.opacity = '0.5';
+    startBtn.style.cursor = 'auto';
+}
+
+function enableStartBtn() {
+    startBtn.disabled = false;
+    startBtn.style.opacity = '1';
+    startBtn.style.cursor = 'pointer';
+}
+
+function resetMenu() {
+    disableStartBtn();
+    if (session.surveys.length > 0) {
+        session.surveys = [];
+    }
+}
+
+function handleStartBtn() {
+    if (session.surveys.length === 0) {
+        disableStartBtn();
+    } else {
+        enableStartBtn();
+    }
 }
