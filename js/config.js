041e86ff5bedb7b71511daf21a9b6525266b359d
Gerardo Giannetta <gerardo.giannetta@exprivia.com>
2025-01-21 00:38:29 +0100

    Prepared the html/css skeleton for the questionnaires

--- js/config.js
+++ js/config.js
@@ -1,7 +1,10 @@
 class Config {
-    #homeURL = 'https://giannettagerardo.github.io/Athena';
+    /*#homeURL = 'https://giannettagerardo.github.io/Athena';
     #surveyURL = 'https://giannettagerardo.github.io/Athena/survey.html';
-    #dashboardURL = 'https://giannettagerardo.github.io/Athena/dashboard.html';
+    #dashboardURL = 'https://giannettagerardo.github.io/Athena/dashboard.html';*/
+    #homeURL = 'http://127.0.0.1:5500';
+    #surveyURL = 'http://127.0.0.1:5500/survey.html';
+    #dashboardURL = 'http://127.0.0.1:5500/dashboard.html';
 
     get homeURL() { return this.#homeURL }
     get surveyURL() { return this.#surveyURL }
