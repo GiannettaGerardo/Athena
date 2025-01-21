659c3371fab38e8ac355e50bb5119b28d2c793a1
Gerardo Giannetta <gerardo.giannetta@exprivia.com>
2025-01-21 23:14:33 +0100

    Added a stepper/tab for handling surveys

---
+++ js/stepper.js
@@ -0,0 +1,56 @@
+export default class Stepper {
+    #stepper;
+    #currentIdx;
+
+    constructor(elements) {
+        this.#stepper = document.getElementById('stepper');
+        if (! this.#stepper.classList.contains('progressBar')) {
+            this.#stepper.setAttribute('class', 'progressBar');
+        }
+        this.#currentIdx = 0;
+
+        let first = true;
+        for (const element of elements) {
+            const li = document.createElement('li');
+            li.innerText = element;
+            if (first) {
+                li.setAttribute('class', 'active not-complete');
+                first = false;
+            }
+            else {
+                li.setAttribute('class', 'not-complete');
+            }
+            li.addEventListener('click', (ev) => {this.#switchActive(ev.target)});
+            this.#stepper.appendChild(li);
+        }
+    }
+
+    #switchActive(target) {
+        const nodes = this.#stepper.childNodes;
+        const size = nodes.length;
+        for (let i = 0; i < size; ++i) {
+            if (nodes[i] === target) {
+                nodes[i].setAttribute('class', 'active not-complete');
+                this.#currentIdx = i;
+                continue;
+            }
+            nodes[i].setAttribute('class', 'not-complete');
+        } 
+    }
+    
+    next() {
+        const nodes = this.#stepper.childNodes;
+        if (this.#currentIdx >= nodes.length) {
+            return false;
+        }
+        nodes[this.#currentIdx++].setAttribute('class', 'active');
+        if (this.#currentIdx < nodes.length) {
+            nodes[this.#currentIdx].setAttribute('class', 'not-complete');
+        }
+        return true;
+    }
+
+    getCurrentIndex() {
+        return this.#currentIdx;
+    }
+}
--- js/survey.js
+++ js/survey.js
@@ -1,13 +1,25 @@
 import { CONFIG } from './config.js';
 import { STORAGE, SESSION } from './store.js';
+import Stepper from './stepper.js'; 
 
 const contextBtn = document.getElementById('context-btn');
 const sessionIdInput = document.getElementById('session-id');
 const creationInput = document.getElementById('creation-input');
 const lastUpdateInput = document.getElementById('lastupdate-input');
+let stepper;
 
-setContextBtn();
+initialize();
 
 function setContextBtn() {
     contextBtn.innerText = STORAGE.contextId;
+}
+
+function initialize() {
+    const surveys = SESSION.getSurveys();
+    if (surveys?.length !== 0) {
+        stepper = new Stepper(surveys);
+        setContextBtn();
+    } else {
+        window.location.href = CONFIG.homeURL;
+    }
 }
