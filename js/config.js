f77549438ae36afc3c1351c007bba067f2013f30
GiannettaGerardo <ggp00519@protonmail.com>
2025-01-21 23:17:57 +0100

    Added a stepper/tab for handling surveys

--- js/config.js
+++ js/config.js
@@ -1,10 +1,7 @@
 class Config {
-    /*#homeURL = 'https://giannettagerardo.github.io/Athena';
-    #surveyURL = 'https://giannettagerardo.github.io/Athena/survey.html';
-    #dashboardURL = 'https://giannettagerardo.github.io/Athena/dashboard.html';*/
-    #homeURL = 'http://127.0.0.1:5500';
-    #surveyURL = 'http://127.0.0.1:5500/survey.html';
-    #dashboardURL = 'http://127.0.0.1:5500/dashboard.html';
+    #homeURL = '/';
+    #surveyURL = '/survey.html';
+    #dashboardURL = '/dashboard.html';
 
     get homeURL() { return this.#homeURL }
     get surveyURL() { return this.#surveyURL }
